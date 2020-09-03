e2f9463e83863acb4208fd9172026d5e59c37267
Nirali Rojivadiya <niralirojivadiya2829@gmail.com>
2020-09-03 12:36:22 +0530

    feat: updated as per comment

--- tests/utils/dir-is-empty.test.js
+++ tests/utils/dir-is-empty.test.js
@@ -1,9 +1,26 @@
-test('dir is empty', () => {
+const fs = require('fs');
+const path = require('path');
+const dirToCreate = path.join(process.cwd(), 'temp');
+
+test('dir is not empty', () => {
     const dirIsEmpty = require('utils/dir-is-empty.js');
     expect(!dirIsEmpty(process.cwd())).toBeTruthy();
 });
 
-test('dir is not empty', () => {
+test('dir is empty', () => {
     const dirIsEmpty = require('utils/dir-is-empty.js');
-    expect(dirIsEmpty(process.cwd())).toBeFalsy();
+    if (fs.existsSync(dirToCreate)) {
+        expect(dirIsEmpty(dirToCreate)).toBeTruthy();
+        fs.rmdirSync(dirToCreate);
+    } else {
+        fs.mkdir(dirToCreate, {mode: '0777'},
+            (err) => {
+                if (err) {
+                    throw err;
+                }
+                expect(dirIsEmpty(dirToCreate)).toBeTruthy();
+                fs.rmdirSync(dirToCreate);
+            }
+        );
+    }
 });
