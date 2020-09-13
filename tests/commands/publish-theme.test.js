aceb717cb81fe1e53838a237b7c6a5d3251336ca
Nirali Rojivadiya <niralirojivadiya2829@gmail.com>
2020-09-13 11:56:22 +0530

    feat: mock rc with siteUrl and adminApiKey for publish-test

--- tests/commands/publish-theme.test.js
+++ tests/commands/publish-theme.test.js
@@ -1,23 +1,36 @@
-const axios = require('axios');
+const cosmiconfig = require('cosmiconfig');
 const fs = require('fs');
+const axios = require('axios');
 const token = require('@tryghost/admin-api/lib/token');
 const FormData = require('form-data');
 
 test('publish theme', async () => {
+    const sync = {
+        search: () => ({
+            config: {siteUrl: 'http://localhost:2368', adminApiKey: 'abc:xyz'}
+        })
+    };
+    jest.spyOn(cosmiconfig, 'cosmiconfigSync').mockImplementation(() => sync);
+    
     const publishTheme = require('commands/publish-theme');
     expect(publishTheme.command).toContain('publish-theme');
     expect(publishTheme.describe).toBeTruthy();
+
     const argv = {path: process.cwd()};
     const path = argv.path;
-    const siteUrl = 'http://127.0.0.1:2368/';
-    const adminApiKey = 'abc:xyz';
+    const rc = require('../../utils/rc.js');
+    const siteUrl = rc.require('siteUrl');
+    const adminApiKey = rc.require('adminApiKey');
     const version = process.env.API_VERSION || 'v3';
+    
     let formData = new FormData();
-    formData.append('file', fs.createReadStream(path));    
+    formData.append('file', fs.createReadStream(path));
+
     const url = `${siteUrl}/ghost/api/${version}/admin/themes/upload/`;
     if (argv.verbose) {
         console.log(`Uploading ${path} to ${url}`);
     }
+
     await axios.post(url,
         formData,
         {
@@ -27,9 +40,12 @@ test('publish theme', async () => {
             }
         }
     )
-        .then((response) => {
-            console.log(response);
-        }).catch ((error) => {
-            console.log(error);
+        .then(function () {
+            if (argv.verbose) {
+                console.log('done.');
+            }
+        })
+        .catch(function (error) {
+            console.error(error.toString());
         });
 });
