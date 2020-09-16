0840c882424cd52bf5e2d2e3c704b6b54c876e60
Nirali Rojivadiya <niralirojivadiya2829@gmail.com>
2020-09-16 23:04:13 +0530

    publish theme test case

--- tests/commands/publish-theme.test.js
+++ tests/commands/publish-theme.test.js
@@ -1,8 +1,6 @@
 const cosmiconfig = require('cosmiconfig');
-const fs = require('fs');
 const axios = require('axios');
-const token = require('@tryghost/admin-api/lib/token');
-const FormData = require('form-data');
+jest.mock('axios');
 
 test('publish theme', async () => {
     const sync = {
@@ -15,37 +13,15 @@ test('publish theme', async () => {
     const publishTheme = require('commands/publish-theme');
     expect(publishTheme.command).toContain('publish-theme');
     expect(publishTheme.describe).toBeTruthy();
-
-    const argv = {path: process.cwd()};
-    const path = argv.path;
-    const rc = require('../../utils/rc.js');
-    const siteUrl = rc.require('siteUrl');
-    const adminApiKey = rc.require('adminApiKey');
-    const version = process.env.API_VERSION || 'v3';
-    
-    let formData = new FormData();
-    formData.append('file', fs.createReadStream(path));
-
-    const url = `${siteUrl}/ghost/api/${version}/admin/themes/upload/`;
-    if (argv.verbose) {
-        console.log(`Uploading ${path} to ${url}`);
-    }
-
-    await axios.post(url,
-        formData,
-        {
-            headers: {
-                Authorization: `Ghost ${token(version, adminApiKey)}`,
-                ...formData.getHeaders()
-            }
-        }
-    )
-        .then(function () {
-            if (argv.verbose) {
-                console.log('done.');
-            }
-        })
-        .catch(function (error) {
-            console.error(error.toString());
-        });
+    const theme = {
+        themes: [{
+            name: 'Test',
+            package: {
+                name: 'Test'
+            },
+            active: false
+        }]
+    };
+    const resp = {data: theme};
+    axios.post.mockResolvedValue(resp);
 });
